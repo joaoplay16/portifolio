4e79ed426ec270eebb2b371d00b82af394f449b7
João Pedro <joaoplay16@gmail.com>
2022-07-11 12:50:06 -0300

    LinkButtonWithIcon redundant style removed

--- src/pages/comics-info/comics-info.js
+++ src/pages/comics-info/comics-info.js
@@ -213,33 +213,7 @@ const LinkButtonWithIcon = styled("div")`
   display: flex;
   gap: 8px;
   align-items: flex-end;
-
-  a {
-    align-self: center;
-    margin-top: 8px;
-    padding: 8px 0px;
-    position: relative;
-  }
-
-  a::after {
-    content: "";
-    display: block;
-    position: absolute;
-    background-color: var(--cor-0);
-    width: 100%;
-    height: 2px;
-    top: 25px;
-  }
-
-  a::before {
-    content: "";
-    display: block;
-    position: absolute;
-    width: 32px;
-    height: 32px;
-    bottom: 0px;
-    left: -40px;
-  }
 `
+
 
 export default ComicsInfo
